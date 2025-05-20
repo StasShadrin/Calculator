7dc8a09c879272e44f410e87d1fdb3a075e178a0
Stas <stas.shad.dev@gmail.com>
2025-05-20 15:21:42 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,56 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите арифметическое выражение (например, 1 + 2):");
+
+            while (true) {
+                System.out.println(calc(br.readLine()));
+            }
+        } catch (Exception e) {
+            System.out.println("throws Exception");
+        }
+    }
+
+    static String calc(String input) {
+        String[] parts = input.split(" ");
+
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("Неверный формат ввода. Ожидается: 'число оператор число'.");
+        }
+
+        int a = parseNumber(parts[0]);
+        String operator = parts[1];
+        int b = parseNumber(parts[2]);
+
+        return switch (operator) {
+            case "+" -> String.valueOf(a + b);
+            case "-" -> String.valueOf(a - b);
+            case "*" -> String.valueOf(a * b);
+            case "/" -> {
+                if (b == 0) {
+                    throw new ArithmeticException("Деление на ноль.");
+                }
+                yield String.valueOf(a / b);
+            }
+            default -> throw new IllegalArgumentException("Неверный оператор. Ожидается: +, -, *, /.");
+        };
+    }
+
+    static int parseNumber(String str) {
+        try {
+            int number = Integer.parseInt(str);
+            if (number < 1 || number > 10) {
+                throw new IllegalArgumentException("Число должно быть от 1 до 10 включительно.");
+            }
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Неверный ввод. Ожидается целое число.");
         }
     }
 }
